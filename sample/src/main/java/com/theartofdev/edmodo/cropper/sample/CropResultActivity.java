9c58c94c711734c1f60910f99224190ee7439775
Brill Pappin <brill@pappin.ca>
2016-04-16 18:36:55 +0300

    Added checkerboard backdrop for testing ticket #50, and left it in as useful. (cherry picked from commit 993a58b)

--- sample/src/main/java/com/theartofdev/edmodo/cropper/sample/CropResultActivity.java
+++ sample/src/main/java/com/theartofdev/edmodo/cropper/sample/CropResultActivity.java
@@ -30,6 +30,8 @@ public final class CropResultActivity extends Activity {
      */
     static Bitmap mImage;
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +39,9 @@ public final class CropResultActivity extends Activity {
         setContentView(R.layout.activity_crop_result);
 
         if (mImage != null) {
-            ((ImageView) findViewById(R.id.resultImageView)).setImageBitmap(mImage);
+            imageView = ((ImageView) findViewById(R.id.resultImageView));
+            imageView.setBackgroundResource(com.theartofdev.edmodo.cropper.R.drawable.backdrop);
+            imageView.setImageBitmap(mImage);
             double ratio = ((int) (10 * mImage.getWidth() / (double) mImage.getHeight())) / 10d;
             int byteCount = 0;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1) {
