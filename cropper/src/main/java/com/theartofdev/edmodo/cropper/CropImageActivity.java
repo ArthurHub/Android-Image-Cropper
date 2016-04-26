fe4b83f0e153b904e2776f57e669057b2ee019f6
Arthur <arthur@tonkean.com>
2016-04-26 15:53:23 +0300

    add CropViewActivity WIP

---
+++ cropper/src/main/java/com/theartofdev/edmodo/cropper/CropImageActivity.java
@@ -0,0 +1,77 @@
+// "Therefore those skilled at the unorthodox
+// are infinite as heaven and earth,
+// inexhaustible as the great rivers.
+// When they come to an end,
+// they begin again,
+// like the days and months;
+// they die and are reborn,
+// like the four seasons."
+//
+// - Sun Tsu,
+// "The Art of War"
+
+package com.theartofdev.edmodo.cropper;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * TODO:a add doc
+ */
+public class CropImageActivity extends Activity {
+
+    /**
+     * The crop image view library widget used in the activity
+     */
+    private CropImageView mCropImageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crop_image_activity);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Intent intent = getIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.crop_image_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.crop_image_menu_crop) {
+            return true;
+        }
+        if (item.getItemId() == R.id.crop_image_menu_rotate) {
+            mCropImageView.rotateImage(90);
+            return true;
+        }
+        if (item.getItemId() == android.R.id.home) {
+            cancel();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        cancel();
+    }
+
+    private void cancel() {
+        finish();
+    }
+}
+
